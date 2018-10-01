546c2ecf2635d5144ceec04c59db3fa5d7ca52a8
n.plachta <n.plachta@microsoft.wsei.edu.pl>
2018-10-01 19:09:10 +0200

    starting to design tests

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using console-calculator;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestDodawania()
+        {
+            double a = 10;
+            double b = 20;
+            double expectedResult = 30;
+
+            double actualResult = wynik;
+
+            Assert.AreEqual(expectedResult, actualResult);
+
+
+        }
+        [TestMethod]
+        public void TestOdejmowania()
+        {
+
+        }
+        [TestMethod]
+        public void TestMnozenia()
+        {
+
+        }
+        [TestMethod]
+        public void TestDzielenia()
+        {
+
+        }
+    }
+}
---
+++ UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestDodawania()
+        {
+            double a = 10;
+            double b = 20;
+            double expectedResult = 30;
+            
+            double actualResult = ;
+
+            Assert.AreEqual(expectedResult, actualResult);
+
+
+        }
+        [TestMethod]
+        public void TestOdejmowania()
+        {
+            double a = 20;
+            double b = 10;
+            double expectedResult = 10;
+
+            double actualResult = ;
+
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+        [TestMethod]
+        public void TestMnozenia()
+        {
+            double a = 5;
+            double b = 4;
+            double expectedResult = 20;
+
+            double actualResult = ;
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [TestMethod]
+        public void TestDzielenia()
+        {
+            double a = 20;
+            double b = 5;
+            double expectedResult = 4;
+
+            double actualResult = ;
+
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+    }
+}
